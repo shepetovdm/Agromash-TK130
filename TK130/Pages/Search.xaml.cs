325d8a103ffee1b75f72e95ddc18384ab73b4659
shepetovdm <shepetovdm@gmail.com>
2015-12-21 23:24:18 +0300

    Тема: Страничка "Поиск" Сущность: Соединение с БД, верстка страницы, работа с DATA GRID

--- TK130/Pages/Search.xaml.cs
+++ TK130/Pages/Search.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+
+
 
 namespace TK130.Pages
 {
@@ -20,9 +25,166 @@ namespace TK130.Pages
     /// </summary>
     public partial class Search : UserControl
     {
+
+        string mDbPath = "catalog-db.db";
+
+        SQLiteConnection mConn;
+        SQLiteDataAdapter mAdapter;
+        DataTable mTable;
+        
         public Search()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+    /*        
+  // Navigate to HTML document stream
+
+
+           //  wbSample.Navigate(@".\Docs\3511.html");
+
+            mConn = new SQLiteConnection("Data Source=" + mDbPath);
+
+            // ----------------- Opening The Connection -----------------
+            // ----------------------------------------------------------
+            // I.e. Opening DB's File for Reading And Writing.
+            // SQLiteDataAdapter cans do it automatically.
+            // But, if you would also use SQLiteCommand, or GetSchema(),
+            // you should Open DB Manually.
+            mConn.Open();
+         
+
+            // ---------- Creating A Test Table, If Not Exists ----------
+            // ----------------------------------------------------------
+            // id        - Unique Counter - Key Field (Required in any table)
+            // FirstName - Text
+            // Age       - Integer
+          
+            // ---------- Get All Tables From DB to ComboBox -----------
+            // ---------------------------------------------------------
+            // There "Tables" is a system table which contains info
+            // about tables in DB.
+            // "TABLE_NAME" field in "Tables" contains table names.
+            /*
+             using (DataTable dt = mConn.GetSchema("Tables"))
+             {
+                 CmbTables.ItemsSource = dt.DefaultView;
+                 if (CmbTables.Items.Count > 0)
+                 {
+                     CmbTables.SelectedIndex = 0;
+                 }
+             }
+         }
+
+         private void BtnSelectTable_Click(object sender, RoutedEventArgs e)
+         {
+             // --- Putting All Data From Selected Table To DataTable ---
+             // ---------------------------------------------------------
+             // In simply put, DataTable is just matrix (2-dimensional array)
+             // which stores data of the table.
+             mAdapter = new SQLiteDataAdapter("SELECT * FROM search", mConn);
+             mTable = new DataTable(); // Don't forget initialize!
+             mAdapter.Fill(mTable);
+
+             // ---------- Disabling Counter Field For Edition ----------
+             // ---------------------------------------------------------
+             // Because it can throw exception.
+             if (mTable.Columns.Contains("id"))
+             {
+                 mTable.Columns["id"].ReadOnly = true;
+             }
+
+             // ----------- Fixing One Bug Of WPF's DataGrid ------------
+             // ---------------------------------------------------------
+             // If DataTable is empty, DataGrid not show columns and
+             // not allows to add a row.
+             // To fix, you should add at least 1 row to empty DataTable.
+             if (mTable.Rows.Count == 0)
+             {
+                 mTable.Rows.Add(new object[mTable.Columns.Count]);
+             }
+
+             // ------------ Making DataBase Saving Changes -------------
+             // ---------------------------------------------------------
+             // SQLiteCommandBuilder authomatically generates
+             // neccessary INSERT, UPDATE, DELETE SQL queries.
+             // Next we just have to run the
+             // mAdapter.Update(mTable);
+             // and all changes in the table will be saved to DataBase.
+             new SQLiteCommandBuilder(mAdapter);
+
+             // ------------- Binding DataTable To DataGrid -------------
+             // ---------------------------------------------------------
+             // DataGrid visualizes DataTable's data in the window.
+             dataGrid1.ItemsSource = mTable.DefaultView;
+         }
+
+         private void dataGrid1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+         {
+
+         }
+
+     }
+ */
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+
+            mConn = new SQLiteConnection("Data Source=" + mDbPath);
+
+            // ----------------- Opening The Connection -----------------
+            // ----------------------------------------------------------
+            // I.e. Opening DB's File for Reading And Writing.
+            // SQLiteDataAdapter cans do it automatically.
+            // But, if you would also use SQLiteCommand, or GetSchema(),
+            // you should Open DB Manually.
+            mConn.Open();
+            // --- Putting All Data From Selected Table To DataTable ---
+            // ---------------------------------------------------------
+            // In simply put, DataTable is just matrix (2-dimensional array)
+            // which stores data of the table.
+            string search_query = "SELECT search_label as Наименование, search_sku as обозначение FROM search WHERE search_label like \"" + Search_Text.Text + "\"";
+            mAdapter = new SQLiteDataAdapter(search_query, mConn);
+            mTable = new DataTable(); // Don't forget initialize!
+            mAdapter.Fill(mTable);
+
+            // ---------- Disabling Counter Field For Edition ----------
+            // ---------------------------------------------------------
+            // Because it can throw exception.
+            if (mTable.Columns.Contains("search_label"))
+            {
+                mTable.Columns["search_label"].ReadOnly = true;
+            }
+
+            // ----------- Fixing One Bug Of WPF's DataGrid ------------
+            // ---------------------------------------------------------
+            // If DataTable is empty, DataGrid not show columns and
+            // not allows to add a row.
+            // To fix, you should add at least 1 row to empty DataTable.
+            if (mTable.Rows.Count == 0)
+            {
+                mTable.Rows.Add(new object[mTable.Columns.Count]);
+            }
+
+            // ------------ Making DataBase Saving Changes -------------
+            // ---------------------------------------------------------
+            // SQLiteCommandBuilder authomatically generates
+            // neccessary INSERT, UPDATE, DELETE SQL queries.
+            // Next we just have to run the
+            // mAdapter.Update(mTable);
+            // and all changes in the table will be saved to DataBase.
+            new SQLiteCommandBuilder(mAdapter);
+
+            // ------------- Binding DataTable To DataGrid -------------
+            // ---------------------------------------------------------
+            // DataGrid visualizes DataTable's data in the window.
+            gridProducts.ItemsSource = mTable.DefaultView;
+            mConn.Close();
+
         }
     }
 }
